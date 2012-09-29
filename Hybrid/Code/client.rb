ae565477874245bc14bce71b13fe3c4e8bbed7c5
Ruben Espinosa <rderoldan1@gmail.com>
2012-09-29 16:30:51 -0500

    merge error

--- Hybrid/Code/client.rb
+++ Hybrid/Code/client.rb
@@ -1,4 +1,46 @@
 require 'drb'
+require 'colorize'
 class Client
+  include DRbUndumped
+
+  def initialize(server,player)
+    DRb.start_service("druby://localhost:0")
+    @server, @player = server, player
+    log("Login on server at #{server}",1)
+  end
+
+  def run
+     @server.login(self)
+
+  end
+
+  def log(message,code)
+    time = Time.now
+    if code.eql? 0
+      puts "[Server Info] #{message} at #{time.strftime("%a %d, %b %Y")}".green
+    elsif code.eql? 1
+      puts "#{message} at #{time.strftime("%a %d, %b %Y")}".blue
+    end
+
+  end
+
+  def get_name
+    @player
+  end
+
+  def set_opponent(user)
+    @opponent = user
+    @opponent.log("Hello I'm #{@player}",1)
+  end
 
 end
+
+if ARGV.length.eql? 2
+  server_uri = 'druby://localhost:' +  ARGV[1]
+else
+  server_uri = 'druby://localhost:4000'
+end
+server = DRbObject.new_with_uri(server_uri)
+client = Client.new(server,"jasok")
+client.run
+DRb.thread.join
--- Hybrid/Code/server.rb
+++ Hybrid/Code/server.rb
@@ -1,4 +1,46 @@
 require 'drb'
+require 'colorize'
 class Server
+  include DRbUndumped
+
+  def initialize(port = 4000)
+    puts port
+    DRb.start_service("druby://localhost:#{port}", self)
+    log "Server started: #{DRb.uri}"
+    @users, @match = {},[]
+  end
+
+  def log(message)
+    time = Time.now
+    puts "#{message} at #{time.strftime("%a %d, %b %Y")}".green
+  end
+
+  def login(user)
+    user_name = user.get_name
+    if @users.include? user_name
+        user.log("The name is already taken",0)
+    else
+        user.log("Welcome to the best tic-tac-toc",0)
+        @users[user_name] = user
+        match(user)
+    end
+  end
+
+  def match(user)
+    if @match.count.eql? 1
+      @match[1] = user
+      user.log("The match is going to begin",0)
+      @match[0].set_opponent(user)
+      user.set_opponent(@match[0])
+    else
+      @match[0] = user
+      user.log("Waiting for the opponent",0)
+    end
+  end
+
+
 
 end
+
+server = Server.new()
+DRb.thread.join()
